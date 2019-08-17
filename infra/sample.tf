3a91e6ac11f2a6c7faaf3fef833ae5df32572852
Shigeki Shoji <takesection@gmail.com>
2019-08-18 03:17:55 +0900

    WIP

--- infra/sample.tf
+++ infra/sample.tf
@@ -1,4 +1,6 @@
 variable "bucketname" {}
+variable "hostedzonename" {}
+variable "certificate" {}
 
 provider "aws" {
   region = "ap-northeast-1"
@@ -15,6 +17,10 @@ resource "aws_cloudformation_stack" "sample" {
   name = "sample-private-api-gateway"
   template_url = "https://s3.amazonaws.com/${var.bucketname}/sample.yaml"
   capabilities = ["CAPABILITY_NAMED_IAM"]
+  parameters = {
+    HostedZoneName = "${var.hostedzonename}"
+    Certificate = "${var.certificate}"
+  }
   tags = [
     { TEMPLATE_VER = "${filemd5("sample.yaml")}" }
   ]
